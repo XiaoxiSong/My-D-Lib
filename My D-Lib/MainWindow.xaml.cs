0141595c9b81bebba6b32ea002a7e9b9abb55203
U-stx_song-THINK\stx_song <stx_song@stx_song-THINK.(none)>
2013-05-08 12:01:03 +0800

    init My D-Lib

---
+++ My D-Lib/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace My_D_Lib
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            doubanOAuth.Common.APIKey = "0edbb8fd24e510aa2c932af76eaf31fe";
+            var douban = doubanOAuth.API.AuthGetCode("xiaoxi.song85@gmail.com", "x851012i");
+        }
+    }
+}
